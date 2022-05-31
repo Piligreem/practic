9629c6dc8a4572d35d49ba25cb140596086a95c9
Piligreem <cry.warface@yandex.ru>
2022-06-01 00:00:47 +0300

    main/models.py <- update classes Vendor, Category, Product

--- practic_site/main/models.py
+++ practic_site/main/models.py
@@ -4,15 +4,27 @@ from django.db import models
 class Vendor(models.Model):
     name = models.CharField(default='Noname', max_length=150)
 
+    def __str__(self):
+        return self.name
 
-class Categoty(models.Model):
+
+class Category(models.Model):
     title = models.CharField(default='Untitled', max_length=150)
+
+    def __str__(self):
+        return self.title
 
 
 class Product(models.Model):
     title = models.CharField(default='Untitled', max_length=150)
-    rating = models.IntegerField(default=0)
+    buy_rating = models.IntegerField(default=0)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    category = models.ForeignKey(Categoty, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def buy(self):
+        self.buy_rating += 1
 
 
